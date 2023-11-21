4eb112c1d5cbbb690e6baa44d5718d0c086888c1
romielilucero@gmail.com <romielilucero@gmail.com>
2023-11-21 17:15:22 -0300

    entrega1

--- src/main/java/RecepcionMain.java
+++ src/main/java/RecepcionMain.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class RecepcionMain {
+/*
+Integrante grupo 1:
+
+
+Gonzalo Illescas
+
+María Marta Merino
+
+Romina Elizabeth Lucero
+
+Camilo Quiroga
+
+Juan José Casco
+ */
+
+
+
+
+public class RecepcionMain {
+    public static void main(String[] args) {
+
+        Cliente cliente1=new Cliente();
+        cliente1.ingresoDatos();
+
+        AreaComercial cli= new AreaComercial();
+        cli.mostrarClientes();
+
+
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
